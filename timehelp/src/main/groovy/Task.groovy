5cdce7e828653cec42074c178a4a0815c1c43f14
Ilya Silvestrov <tellary@gmail.com>
2011-09-18 02:49:29 +0400

    Add task modification listeners

--- timehelp/src/main/groovy/Task.groovy
+++ timehelp/src/main/groovy/Task.groovy
@@ -5,16 +5,27 @@ class Task {
   float timeStretch
 	boolean noStretch = false
   StretchModel stretchModel
+  List<TaskModificationListener> taskModificationListeners = []
+
+  private void notifyListeners() {
+    for (TaskModificationListener listener: taskModificationListeners) {
+      listener.taskModified(this)
+    }
+  }
 
   void setTimeStretch(float timeStretch) {
     this.timeStretch = timeStretch
-    if (stretchModel != null)
+    if (stretchModel != null) {
       TimeHelp.stretchTasks(stretchModel)
+      notifyListeners()
+    }
   }
 
   void setNoStretch(boolean noStretch) {
     this.noStretch = noStretch
-    if (stretchModel != null)
+    if (stretchModel != null) {
       TimeHelp.stretchTasks(stretchModel)
+      notifyListeners()
+    }
   }
 }
---
+++ timehelp/src/main/groovy/TaskModificationListener.groovy
@@ -0,0 +1,9 @@
+
+/**
+ * Created by Silvestrov Ilya
+ * Date: 9/18/11
+ * Time: 2:47 AM
+ */
+public interface TaskModificationListener {
+  void taskModified(Task task)
+}
