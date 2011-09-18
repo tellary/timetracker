1d26f2b300d9f253cdaf0232e6b5770907275206
Ilya Silvestrov <tellary@gmail.com>
2011-09-19 01:19:00 +0400

    Wrap project and tasks labels on form

--- timehelp/src/main/groovy/StretchTimeForm.groovy
+++ timehelp/src/main/groovy/StretchTimeForm.groovy
@@ -10,6 +10,9 @@ import java.awt.BorderLayout
 import java.awt.event.ActionEvent
 import javax.swing.JTextField
 import javax.swing.JCheckBox
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
+import javax.swing.JLabel
 
 /**
  * Created by Silvestrov Ilya
@@ -30,12 +33,14 @@ class StretchTimeForm {
     Lock lock = new ReentrantLock()
     lock.lock()
     Condition tasksSelected = lock.newCondition();
+    List<JLabel> taskLabels = []
+    List<JLabel> projectLabels = []
     JFrame frame =
     swing.frame (
         title: 'Select no-stretch tasks',
         defaultCloseOperation: JFrame.EXIT_ON_CLOSE,
         show: true,
-        pack: true,
+        pack: false,
     ) {
       scrollPane {
         panel {
@@ -73,24 +78,32 @@ class StretchTimeForm {
           for (Task task: stretchModel.tasks) {
             panel(border: BorderFactory.createMatteBorder(1, 0, 0, 0, Color.black)) {
               borderLayout()
-              label(text: task.projectName, constraints: BorderLayout.NORTH)
-              panel(constraints: BorderLayout.SOUTH) {
+              projectLabels.add(label(
+                  text: "<html><body width='100%'>$task.projectName</body></html>",
+                  constraints: BorderLayout.NORTH
+              ))
+              panel(constraints: BorderLayout.CENTER) {
                 borderLayout()
                 hbox (constraints: BorderLayout.LINE_START) {
                   label(text: '   ', constraints: BorderLayout.WEST)
-                  label(text: "$task.taskName   ${TimeHelp.floatHoursToString(task.timeSpent)}",
-                      constraints: BorderLayout.CENTER)
+                  taskLabels.add(
+                      label(
+                          text: "<html><body width='100%'>$task.taskName</body></html>",
+                          constraints: BorderLayout.CENTER,
+                      )
+                  )
                 }
                 hbox (constraints: BorderLayout.LINE_END) {
+                  label(text: "${TimeHelp.floatHoursToString(task.timeSpent)}", constraints: BorderLayout.LINE_START)
                   JCheckBox checkBox = checkBox(
-                      constraints: BorderLayout.LINE_START,
+                      constraints: BorderLayout.CENTER,
                       actionPerformed: {Task taskArg, event->
                         println taskArg.taskName
                         taskArg.noStretch = event.source.selected;
                       }.curry(task)
                   )
                   JTextField timeStretchFld = textField(
-                      constraints: BorderLayout.CENTER,
+                      constraints: BorderLayout.LINE_END,
                       columns: 6,
                       actionPerformed: {Task taskArg, ActionEvent event ->
                         JTextField field = (JTextField)event.getSource()
@@ -125,6 +138,30 @@ class StretchTimeForm {
         }
       }
     }
+
+    frame.addComponentListener(new ComponentAdapter() {
+      @Override
+      void componentResized(ComponentEvent e) {
+        int frameWidth = frame.getWidth()
+
+        for (JLabel taskLabel: taskLabels) {
+          String htmlText = taskLabel.getText()
+          htmlText = htmlText.replaceAll("<body width='[^']+'", "<body width='${(int)frameWidth-200}'")
+          taskLabel.setText(htmlText)
+          taskLabel.invalidate()
+        }
+
+        for (JLabel projectLabel : projectLabels) {
+          String htmlText = projectLabel.getText()
+          htmlText = htmlText.replaceAll("<body width='[^']+'", "<body width='${(int)frameWidth-200}'")
+          projectLabel.setText(htmlText)
+          projectLabel.invalidate()
+        }
+      }
+    })
+
+    frame.pack()
+
     tasksSelected.await()
     lock.unlock()
     frame.dispose()
