26c6a6f6e0edadadea40d1c952b841d07d8befe2
Ilya Silvestrov <tellary@gmail.com>
2012-07-23 09:53:05 +0400

    T5.2: Make tree to display aggregate time spent in clock format

--- src/main/java/ru/silvestrov/timetracker/view/activitytree/ActivityJTree.java
+++ src/main/java/ru/silvestrov/timetracker/view/activitytree/ActivityJTree.java
@@ -1,5 +1,6 @@
 package ru.silvestrov.timetracker.view.activitytree;
 
+import ru.silvestrov.timetracker.model.activitycontrollist.TimeHelp;
 import ru.silvestrov.timetracker.model.activitytree.ActivityTree;
 import ru.silvestrov.timetracker.model.activitytree.ActivityTreeNode;
 
@@ -19,7 +20,7 @@ public class ActivityJTree extends JTree {
     public String convertValueToText(Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
         if (value instanceof ActivityTreeNode) {
             ActivityTreeNode node = (ActivityTreeNode) value;
-            return node.getName() + "(" + node.getTimeSpent() + " mins)";
+            return node.getName() + "(" + TimeHelp.formatTime(node.getAggregateTimeSpent()) + ")";
         } else if (value instanceof ActivityTree) {
             return "";
         } else {
